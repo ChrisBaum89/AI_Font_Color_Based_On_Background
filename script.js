a34afdff2ee5b24cecf5492d402f3d9b8268f4ac
Chris Baum <Christopher.Baum@valpo.edu>
2023-04-27 16:39:34 -0400

    added setRandomColor function and getElements

--- script.js
+++ script.js
@@ -13,7 +13,20 @@ data = [
 
 net.train(data)
 
-const diagram = document.getElementById('diagram')
-diagram.innerHTML = brain.utilities.toSVG(net)
+const colorEl = document.getElementById('color');
+const guessEl = document.getElementById('guess');
 
-console.log(net.run([1, 0]))
+const whiteButton = document.getElementById('white-button');
+const blackButton = document.getElementById('black-button');
+const printButton = document.getElementById('print-button');
+
+setRandomColor()
+
+function setRandomColor() {
+    color = {
+        r: Math.random(),
+        g: Math.random(),
+        b: Math.random(),
+    }
+    colorEl.style.backgroundColor = `rgba(${color.r * 255}, ${color.g * 255}, ${color.b * 255})`
+}
